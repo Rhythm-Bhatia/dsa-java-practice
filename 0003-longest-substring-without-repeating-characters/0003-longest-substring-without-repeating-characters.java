f19ff34b590c76590484ffc99d871e91948f2a6c
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2025-08-14 12:20:14 +0530

    Time: 6 ms (73.41%), Space: 44.7 MB (70.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,28 +1,39 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int n=s.length();
+//         int count=0;
+//         Set<Character>set=new LinkedHashSet<>();
+//         for(int i=0;i<n;i++){
+//             while(set.contains(s.charAt(i))){
+//                 char first = set.iterator().next(); 
+//                 set.remove(first); 
+//             }
+//             set.add(s.charAt(i));
+//             count=Math.max(count,set.size());
+//         }
+//         return count;
+//     }
+// }
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n=s.length();
-        int l=0;
-        int r=0;
-        int count=0;
+        int n = s.length();
+        int maxLength = 0;
+        Set<Character> charSet = new HashSet<>();
+        int left = 0;
         
-
-        Set<Character>set=new LinkedHashSet<>();
-        for(int i=0;i<n;i++){
-            while(set.contains(s.charAt(i))){
-                char first = set.iterator().next(); 
-                set.remove(first); 
-
+        for (int right = 0; right < n; right++) {
+            if (!charSet.contains(s.charAt(right))) {
+                charSet.add(s.charAt(right));
+                maxLength = Math.max(maxLength, right - left + 1);
+            } else {
+                while (charSet.contains(s.charAt(right))) {
+                    charSet.remove(s.charAt(left));
+                    left++;
+                }
+                charSet.add(s.charAt(right));
             }
-            
-            set.add(s.charAt(i));
-            count=Math.max(count,set.size());
-
-            
-
-
-       }
-       return count;
-
-       
+        }
+        
+        return maxLength;
     }
 }
