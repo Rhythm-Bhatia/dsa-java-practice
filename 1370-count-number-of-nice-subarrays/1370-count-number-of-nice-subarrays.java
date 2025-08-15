0f29d8dbbe7e3d1485c85bfca844c9597125eca7
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2025-08-15 16:03:22 +0530

    Time: 13 ms (62.36%), Space: 55.3 MB (37.56%) - LeetHub

---
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        //find number of subarrays with sum less than or equal to k
+        //and number of subarray with sum less than equal to k-1
+        
+        int result=countSubArray(nums,k)-countSubArray(nums,k-1);
+        return result;
+    }
+        private int countSubArray(int []nums,int k){
+            int r=0;
+            int l=0;
+            int count=0;
+            int n=nums.length;
+            int ans=0;
+            while(r<n){
+                if(nums[r]%2!=0){
+                    count++;
+                }
+                while(count>k){
+                    if(nums[l]%2==0){
+                        l++;
+                    }
+                    else{
+                        count--;
+                        l++;
+                    }
+                }
+                ans=ans+(r-l+1);
+                r++;
+                
+                
+                
+            
+                
+
+                
+            }
+            return ans;
+            
+
+        }
+        
+
+
+    
+}
