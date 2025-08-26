3c1d32ccac52b370ce61e50682e8a81b9774bfa1
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2025-08-26 09:12:57 +0530

    Time: 3 ms (13.75%), Space: 56 MB (54.01%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int arr[]=new int[3];
+        for(int i=0;i<bills.length;i++){
+            if(bills[i]==5){
+                arr[0]++;
+            }
+            else if(bills[i]==10){
+                if(arr[0]!=0){
+                    arr[0]--;
+                }
+                else{
+                    return false;
+                }
+                arr[1]++;
+
+            }
+            else if(bills[i]==20){
+                if(arr[1]>=1&&arr[0]>=1){
+                    arr[1]--;
+                    arr[0]--;
+                    arr[2]++;
+                }
+                else if(arr[0]>=3){
+                    arr[0]=arr[0]-3;
+                    arr[2]++;
+                    
+                }
+                else{
+                    return false;
+                }
+
+                
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
