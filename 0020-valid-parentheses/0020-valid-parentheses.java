24014335e248c36e327b941f6200309b40ec61b8
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2025-08-29 20:30:19 +0530

    Time: 4 ms (12.22%), Space: 42.1 MB (21.07%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stack=new Stack();
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
+                stack.push(s.charAt(i));
+            }
+            else if(!stack.isEmpty()){
+                if(s.charAt(i)==')'&&stack.peek()=='('){
+                stack.pop();
+                }
+                else if(s.charAt(i)=='}'&&stack.peek()=='{'){
+                    stack.pop();
+                }
+                else if(s.charAt(i)==']'&&stack.peek()=='['){
+                    stack.pop();
+                }
+                else stack.push(s.charAt(i));
+            }
+            
+            else stack.push(s.charAt(i));
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
