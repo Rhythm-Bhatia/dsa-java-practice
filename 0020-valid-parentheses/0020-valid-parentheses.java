28c4cae84a3fc256c227479346f1702dc1185cc3
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2025-08-29 20:36:14 +0530

    Time: 2 ms (96.6%), Space: 41.8 MB (49.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,31 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character>stack=new Stack();
-        
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
-                stack.push(s.charAt(i));
+        if(s.length()==1)return false;
+        Stack<Character>stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='('||c=='{'||c=='['){
+                stack.push(c);
             }
-            else if(!stack.isEmpty()){
-                if(s.charAt(i)==')'&&stack.peek()=='('){
-                stack.pop();
-                }
-                else if(s.charAt(i)=='}'&&stack.peek()=='{'){
-                    stack.pop();
-                }
-                else if(s.charAt(i)==']'&&stack.peek()=='['){
-                    stack.pop();
-                }
-                else stack.push(s.charAt(i));
-            }
-            
-            else stack.push(s.charAt(i));
+              else{
+                if(stack.isEmpty()) return false; 
+                char top=stack.pop();
+                 if((top=='[' && c!=']')) {
+                    return false;
+                    }
+                    if(top=='{' && c!='}') return false;
+                    if(top=='(' && c!=')') return false;
+               
+              }
         }
-        if(stack.isEmpty()){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return stack.isEmpty();
     }
 }
