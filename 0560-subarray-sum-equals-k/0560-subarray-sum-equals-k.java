7d79700292b1e5b52bba3f7af31d7a3cfd45bbfa
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2025-09-03 12:29:53 +0530

    Time: 23 ms (92.71%), Space: 46.5 MB (65.88%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,43 +1,62 @@
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        // int sum=0;
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum=sum+nums[j];
-                if(sum==k){
-                    count++;
-                }
-            }
-        }
-        return count;
-
-    }
-
-}
-// class Solution{
-//     public int subarraySum(int[]nums,int k){
-//         int start=0;
-//         int end=0;
-//         int sum=0;
-//         while(end<=nums.length && start<=end){
-//             sum=sum+nums[end];
-
-            
-//             if(sum==k){
-//                 count++;
-                
-//             }
-//             while(sum>k){
-//                 sum=sum-nums[start];
-//                 start++;
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         // int sum=0;
+//         int count=0;
+//         for(int i=0;i<nums.length;i++){
+//             int sum=0;
+//             for(int j=i;j<nums.length;j++){
+//                 sum=sum+nums[j];
 //                 if(sum==k){
 //                     count++;
 //                 }
 //             }
-//             end++;
 //         }
+//         return count;
+
+//     }
+
+// }
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         int left = 0, sum = 0, count = 0;
+
+//         for (int right = 0; right < nums.length; right++) {
+//             sum += nums[right];
+
+//             // Shrink from left if sum too big
+//             while (sum > k && left <= right) {
+//                 sum -= nums[left];
+//                 left++;
+//             }
+
+//             // Check if sum == k
+//             if (sum == k) {
+//                 count++;
+//             }
+//         }
+
 //         return count;
 //     }
 // }
+class Solution{
+    public int subarraySum(int[]nums,int k){
+        int prefixSum=0;
+        int count=0;
+        Map<Integer,Integer>map=new HashMap<>();
+        map.put(0,1);
+        
+        for(int i=0;i<nums.length;i++){
+
+            prefixSum=nums[i]+prefixSum;
+            
+
+            if(map.containsKey(prefixSum-k)){
+                count=count+map.get(prefixSum-k);
+            }
+            map.put(prefixSum,map.getOrDefault(prefixSum,0)+1);
+
+
+        }
+        return count;
+    }
+}
