8aa57d9343e06c022092db1d8884fd71fc11fe6c
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2025-09-10 21:55:01 +0530

    Time: 222 ms (94.39%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,6 +1,55 @@
-# Write your MySQL query statement belo
-select a.machine_id,ROUND(AVG(b.timestamp-a.timestamp),3)as processing_time
-from Activity as a Join Activity as b
-on a.machine_id=b.machine_id AND a.process_id=b.process_id
-where a.activity_type='start' AND b.activity_type='end'
-group by a.machine_id
+-- # Write your MySQL query statement belo
+-- -- select a.machine_id,ROUND(AVG(b.timestamp-a.timestamp),3)as processing_time
+-- -- from Activity as a Join Activity as b
+-- -- on a.machine_id=b.machine_id AND a.process_id=b.process_id
+-- -- where a.activity_type='start' AND b.activity_type='end'
+-- -- group by a.machine_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select a.machine_id,round(avg(b.timestamp-a.timestamp),3) as processing_time
+from Activity as a join Activity as b
+on a.machine_id=b.machine_id && a.process_id=b.process_id
+where a.activity_type='start' and b.activity_type='end'
+group by a.machine_id;
