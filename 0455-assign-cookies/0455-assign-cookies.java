e297a9b11d6f0af2cf0d3fe0f2e95d6280e231a5
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2025-12-13 09:51:14 +0530

    Time: 14 ms (67.83%), Space: 51.7 MB (7.81%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,27 +1,20 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        if(s.length==0||g.length==0){
-            return 0;
-        }
-        int count=0;
         Arrays.sort(g);
         Arrays.sort(s);
-        List<Integer>slist=new ArrayList<>();
-        for(int i=0;i<s.length;i++){
-            slist.add(s[i]);
-        }
-
+        int count=0;
+        int j=0;
         for(int i=0;i<g.length;i++){
-            int greedy=g[i];
-            if(slist.size()==0)break;
-            if(greedy>slist.get(slist.size()-1))break;
-            for(int j=0;j<slist.size();j++){
-                if(slist.get(j)>=greedy){
+            while(j<s.length){
+                if(s[j]>=g[i]){
                     count++;
-                    slist.remove(j);
+                    j++;
                     break;
                 }
-
+                else{
+                    j++;
+                }
+                
             }
         }
         return count;
