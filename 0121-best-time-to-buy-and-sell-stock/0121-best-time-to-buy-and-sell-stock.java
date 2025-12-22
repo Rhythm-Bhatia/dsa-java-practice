8121f0214273a02d4426c4aad1d7e5e56ce258c2
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2025-12-22 17:33:49 +0530

    Time: 3 ms (7.07%), Space: 94.4 MB (55.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int sell=prices[prices.length-1];
+        if(prices.length==0)return 0;
         int max=0;
-        int profit=0;
-        for(int i=prices.length-2;i>=0;i--){
-            if(prices[i]>sell){
-                sell=prices[i];
-            }
-            else if(prices[i]<sell){
-                profit=sell-prices[i];
-                max=Math.max(profit,max);
-            }
+        int min=prices[0];
+        for(int i=1;i<prices.length;i++){
+            min=Math.min(min,prices[i]);
+            int ans=prices[i]-min;
+            max=Math.max(max,ans);
 
-            
         }
         return max;
     }
