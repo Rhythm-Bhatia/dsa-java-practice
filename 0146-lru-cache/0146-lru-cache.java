d6ee4cdabbaf032854695cd248f52250c9ab43d5
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-01-24 22:22:09 +0530

    Time: 999 ms (5%), Space: 132 MB (34.02%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,85 +1,49 @@
+import java.util.*;
+
 class LRUCache {
-    class Node{
-        int key;
-        int value;
-        Node next;
-        Node prev;
-        Node(int key,int value){
-            this.key=key;
-            this.value=value;
-        }
-        
-        
-        
-    }
+
     private int capacity;
-    private Node head;
-    private Node tail;
-    private Map<Integer,Node>map=new HashMap<>();
+    private Map<Integer, Integer> map;   // key -> value
+    private LinkedList<Integer> dll;     // keys in LRU order
 
     public LRUCache(int capacity) {
-        this.capacity=capacity;
-        head=new Node(0,0);
-        tail=new Node(0,0);
-        head.next=tail;
-        tail.prev=head;
-        
+        this.capacity = capacity;
+        map = new HashMap<>();
+        dll = new LinkedList<>();
     }
-    
+
     public int get(int key) {
-        if(!map.containsKey(key)){
+        if (!map.containsKey(key)) {
             return -1;
         }
-        Node node=map.get(key);
-        functionremove(node);
-        addlast(node);//to do
-        return node.value;
-        
 
+        // move key to most recently used
+        dll.remove((Integer) key); // O(n) internally
+        dll.addLast(key);
 
-
-
-        
-        
+        return map.get(key);
     }
-    
+
     public void put(int key, int value) {
-        if(map.containsKey(key)){
-            Node node=map.get(key);
-            functionremove(node);
-            node.value=value;
-            addlast(node);
+
+        if (map.containsKey(key)) {
+            // update value
+            map.put(key, value);
+
+            // move key to MRU
+            dll.remove((Integer) key);
+            dll.addLast(key);
             return;
-
         }
-        if(map.size()==capacity){
-            Node lru=head.next;
-            functionremove(lru);
-            map.remove(lru.key);
 
+        if (map.size() == capacity) {
+            // remove least recently used
+            int lruKey = dll.removeFirst();
+            map.remove(lruKey);
         }
-        Node node=new Node(key,value);
-        addlast(node);
-        map.put(key,node);
 
-    }
-    public void addlast(Node node){
-        tail.prev.next=node;
-        node.next=tail;
-        node.prev=tail.prev;
-        tail.prev=node;
-        
-    }
-    public void functionremove(Node node){
-        node.prev.next=node.next;
-        node.next.prev=node.prev;
+        // insert new key
+        map.put(key, value);
+        dll.addLast(key);
     }
 }
-
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
