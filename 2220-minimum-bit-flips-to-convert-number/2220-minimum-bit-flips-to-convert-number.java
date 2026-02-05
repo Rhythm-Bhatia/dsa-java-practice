c748e904e27ed821c3f27f5f11ed0a7b13d0a22f
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-02-05 15:24:19 +0530

    Time: 0 ms (100%), Space: 42.4 MB (28.13%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int count=0;
+        for(int i=0;i<32;i++){
+            if(((1<<i)&start)!=((1<<i)&goal)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
