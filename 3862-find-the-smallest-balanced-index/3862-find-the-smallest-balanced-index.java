2e7ec88e1778711e9d384133819fbd0071d35780
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-03-08 12:24:45 +0530

    Time: 13 ms (12.37%), Space: 137.2 MB (98.27%) - LeetHub

---
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int smallestBalancedIndex(int[] nums) {
+        if(nums.length==1)return -1;
+        int ans=-1;
+        int n=nums.length;
+        long LIMIT = (long)1e14;
+        long numss[]=new long[nums.length];
+        for(int i=0;i<nums.length;i++){
+            numss[i]=(long)nums[i];
+        }
+        long prefix[]=new long[n];
+        long suffix[]=new long[n];
+        prefix[0]=0;
+        suffix[n-1]=1;
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i-1];
+            if(prefix[i]>=LIMIT){
+                // suffix[i]=Integer.MAX_VALUE;
+                prefix[i]=LIMIT+1;
+            }
+        }
+        for(int i=nums.length-2;i>=0;i--){
+            suffix[i]=suffix[i+1]*nums[i+1];
+            if(suffix[i+1] > LIMIT / nums[i+1]){
+                suffix[i] = LIMIT + 1;
+            }
+            else{
+                suffix[i] = suffix[i+1] * nums[i+1];
+            }
+        } 
+        for(int i = 0; i < n; i++){
+            if(prefix[i] == suffix[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
