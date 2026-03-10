e73281e34819af667798b12f4f5d0fd917c8a772
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-03-10 15:55:07 +0530

    Time: 7 ms (64.99%), Space: 47 MB (18.25%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,27 @@
 class Solution {
+    int sum=0;
     public int findTargetSumWays(int[] nums, int target) {
-        // int dp[][]=new int[nums.length][]
-        return target(0,nums,target);
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+        Integer dp[][]=new Integer[nums.length][2*sum+1];
+        return target(0,nums,target,dp,0);
 
     }
-    public int target(int index,int[] nums,int target){
+    public int target(int index,int[] nums,int target,Integer dp[][],int currentSum){
+        // if(Math.abs(target) > sum) return 0; 
         if(index==nums.length){
-            if(target==0){
+            if(currentSum==target){
                 return 1;
             }
             else return 0;
         }
-        int left=target(index+1,nums,target-nums[index]);
-        int right=target(index+1,nums,target+nums[index]);
-        return left+right;
+        if(dp[index][sum+currentSum]!=null){
+            return dp[index][sum+currentSum];
+        }
+        int left=target(index+1,nums,target,dp,currentSum-nums[index]);
+        int right=target(index+1,nums,target,dp,currentSum+nums[index]);
+        return dp[index][sum+currentSum]=left+right;
+
     }
 }
