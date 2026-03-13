e16e16f1ecf722261897ed74fe8c78745ce48eb2
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-03-13 22:01:20 +0530

    Time: 7 ms (71.77%), Space: 46.4 MB (65.04%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int dp[][]=new int[word1.length()+1][word2.length()+1];
+        for(int i=0;i<dp.length;i++)dp[i][0]=0;
+        for(int j=1;j<dp[0].length;j++)dp[0][j]=0;
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        int store=dp[word1.length()][word2.length()];
+        return((word1.length()-store)+(word2.length()-store));
+    }
+}
