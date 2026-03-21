cc90a4fc7b4a6f65aaacd011b71180a3f276430e
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-03-21 11:24:43 +0530

    Time: 16 ms (99.92%), Space: 45.2 MB (53.15%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,40 +1,34 @@
 class Solution {
   public List<Integer> largestDivisibleSubset(int[] nums) {
-    final int n = nums.length;
-    List<Integer> ans = new ArrayList<>();
-    // sizeEndsAt[i] := the maximum size ends in nums[i]
-    int[] sizeEndsAt = new int[n];
-    // prevIndex[i] := the best index s.t.
-    // 1. nums[i] % nums[prevIndex[i]] == 0 and
-    // 2. can increase the size of the subset
-    int[] prevIndex = new int[n];
-    int maxSize = 0; // Max size of the subset
-    int index = -1;  // Track the best ending index
-
-    Arrays.fill(sizeEndsAt, 1);
-    Arrays.fill(prevIndex, -1);
+    int dp[]=new int[nums.length];
     Arrays.sort(nums);
-
-    // Fix the maximum ending number in the subset.
-    for (int i = 0; i < n; ++i) {
-      for (int j = i - 1; j >= 0; --j)
-        if (nums[i] % nums[j] == 0 && sizeEndsAt[i] < sizeEndsAt[j] + 1) {
-          sizeEndsAt[i] = sizeEndsAt[j] + 1;
-          prevIndex[i] = j;
+    Arrays.fill(dp,1);
+    int max=1;
+    int store=0;
+    int prev[]=new int[nums.length];
+    Arrays.fill(prev,-1);
+    for(int i=0;i<nums.length;i++){
+        for(int j=i-1;j>=0;j--){
+            if(nums[i]%nums[j]==0&&dp[j]+1>dp[i]){
+                dp[i]=dp[j]+1;
+                prev[i]=j;
+                if(dp[i]>max){
+                    max=dp[i];
+                    store=i;
+                }
+            }
         }
-      // Find a new subset that has a bigger size.
-      if (maxSize < sizeEndsAt[i]) {
-        maxSize = sizeEndsAt[i];
-        index = i; // Update the best ending index.
-      }
     }
-
-    // Loop from the back to the front.
-    while (index != -1) {
-      ans.add(nums[index]);
-      index = prevIndex[index];
+    
+    List<Integer>list=new ArrayList<>();
+    int i=store;
+    while(i!=-1){
+        list.add(nums[i]);
+        i=prev[i];
     }
+    Collections.reverse(list);
+    return list;
 
-    return ans;
+
   }
 }
