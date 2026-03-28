dd4acc8c934dc4060c9e0f172346a9067a71c199
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-03-28 11:06:22 +0530

    Time: 2308 ms (26.08%), Space: 42.8 MB (81.46%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,34 +1,33 @@
 class Solution {
 
     public int minCut(String s) {
-        int n = s.length();
-        Integer[] dp = new Integer[n];  // memo
-        return solve(0, s, dp) - 1;
+        Integer[]dp=new Integer[s.length()];
+        return solve(s,0,dp)-1;
     }
-
-    private int solve(int i, String s, Integer[] dp) {
-        if (i == s.length()) return 0;
-
-        if (dp[i] != null) return dp[i];
-
-        int minCost = Integer.MAX_VALUE;
-
-        for (int j = i; j < s.length(); j++) {
-            if (isPalindrome(i, j, s)) {
-                int cost = 1 + solve(j + 1, s, dp);
-                minCost = Math.min(minCost, cost);
+    private int solve(String s,int i,Integer[]dp){
+        if(i>=s.length())return 0;
+        if(dp[i]!=null)return dp[i];
+        int min=Integer.MAX_VALUE;
+        for(int j=i;j<s.length();j++){
+            if(isPalindrome(i,j,s)){
+                int temp=1+solve(s,j+1,dp);
+                min=Math.min(min,temp);
             }
         }
-
-        return dp[i] = minCost;
+        return dp[i]=min;
     }
-
-    private boolean isPalindrome(int i, int j, String s) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) return false;
+    private boolean isPalindrome(int i,int j,String s){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+                
+            }
             i++;
             j--;
+
         }
         return true;
     }
+
+    
 }
