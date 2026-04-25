ffc9f0890bcdffc7d1604bc8f64ac7258efa9487
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-04-25 17:14:38 +0530

    Time: 0 ms (100%), Space: 41.1 MB (99.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,16 @@
 class Solution {
-    
     public int climbStairs(int n) {
-        
-        int dp[]=new int[n+1];
-        return climb(dp,n);
-        
+        if(n==0||n==1||n==2){
+            return n;
+        }
+        int ans[]=new int[n];
+        ans[0]=1;
+        ans[1]=2;
+        for(int i=2;i<n;i++){
+            ans[i]=ans[i-1]+ans[i-2];
 
-    }
-    public int climb(int[]dp,int n){
-        if(n==1)return 1;
-        if(n==2)return 2;
-        if(dp[n]!=0)return dp[n];
-        dp[n]=climb(dp,n-1)+climb(dp,n-2);
-        return dp[n];
-
+        }
+        return ans[n-1];
+        
     }
 }
