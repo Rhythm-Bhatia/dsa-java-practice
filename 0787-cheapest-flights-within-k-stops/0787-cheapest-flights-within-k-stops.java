db8a541ceb7bbb3d165dd8576d3c20789f2940f0
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-05-10 17:07:49 +0530

    Time: 6 ms (85.52%), Space: 46.6 MB (37.69%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -3,66 +3,38 @@ class Solution {
     public int findCheapestPrice(int n, int[][] flights,
                                  int src, int dst, int k) {
 
-        List<List<int[]>> adj = new ArrayList<>();
-
-        for(int i = 0; i < n; i++){
-            adj.add(new ArrayList<>());
+        if(src==dst)return 0;
+        List<List<int[]>>list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(new ArrayList<>());
         }
-
-        for(int i = 0; i < flights.length; i++){
-
-            adj.get(flights[i][0]).add(
-                new int[]{flights[i][1], flights[i][2]}
-            );
+        for(int i=0;i<flights.length;i++){
+            list.get(flights[i][0]).add(new int[]{flights[i][1],flights[i][2]});
         }
-
-        int findistance[] = new int[n];
-
-        for(int i = 0; i < n; i++){
-            findistance[i] = (int)1e9;
-        }
-
-        findistance[src] = 0;
-
-        Queue<int[]> pq = new LinkedList<>();
-
-        // {stops, node, distance}
-        pq.add(new int[]{0, src, 0});
-
-        while(!pq.isEmpty()){
-
-            int arr[] = pq.poll();
-
-            int count = arr[0];
-            int element = arr[1];
-            int distance = arr[2];
-
-            if(count > k){
+        Queue<int[]>q=new LinkedList<>();
+        q.add(new int[]{src,0,0});
+        int distance[]=new int[n];
+        Arrays.fill(distance,(int)1e9);
+        distance[src]=0;
+        while(!q.isEmpty()){
+            int arr[]=q.poll();
+            int element=arr[0];
+            int distancee=arr[1];
+            int num=arr[2];
+            if(num>k){
+                q.poll();
                 continue;
             }
-
-            for(int[] a : adj.get(element)){
-
-                int num = a[0];
-                int dis = a[1];
-
-                if(distance + dis < findistance[num]){
-
-                    findistance[num] = distance + dis;
-
-                    pq.add(new int[]{
-                        count + 1,
-                        num,
-                        distance + dis
-                    });
+            for(int []a:list.get(element)){
+                int ele=a[0];
+                int dis=a[1];
+                if(distance[ele]>dis+distancee){
+                    distance[ele]=dis+distancee;
+                    q.add(new int[]{ele,dis+distancee,num+1});
                 }
             }
         }
-
-        if(findistance[dst] == (int)1e9){
-            return -1;
-        }
-
-        return findistance[dst];
+        if(distance[dst]==(int)1e9)return -1;
+        return distance[dst];
     }
 }
