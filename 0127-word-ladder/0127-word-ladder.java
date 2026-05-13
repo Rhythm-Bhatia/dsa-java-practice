49eff7c2fc66ee773838000348e7ed66055f7e12
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-05-13 06:42:30 +0530

    Time: 2763 ms (5.04%), Space: 47.6 MB (85.69%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,47 +1,110 @@
-class Pair{
-    String first;
-    int second;
-    Pair(String first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set = new HashSet<>();
-        for(int i = 0; i<wordList.size(); i++){
-            set.add(wordList.get(i));
+
+    class Pair {
+        String first;
+        String second;
+        int steps;
+
+        Pair(String first, String second, int steps) {
+            this.first = first;
+            this.second = second;
+            this.steps = steps;
         }
-        if(!set.contains(endWord)){
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        if (!wordList.contains(endWord)) {
             return 0;
         }
-        Queue<Pair> queue = new LinkedList<>();
-        queue.offer(new Pair(beginWord, 1));
 
-        while(!queue.isEmpty()){
-            Pair curr = queue.poll();
-            String word = curr.first;
-            int step = curr.second;
+        wordList.add(beginWord);
 
-            if(word.equals(endWord)){
-                return step;
+        int n = wordList.size();
+        int len = beginWord.length();
+
+        // your adjacency list idea
+        List<Pair> adj = new ArrayList<>();
+
+        // make all connections
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                String a = wordList.get(i);
+                String b = wordList.get(j);
+
+                int diff = 0;
+
+                for (int k = 0; k < len; k++) {
+
+                    if (a.charAt(k) != b.charAt(k)) {
+                        diff++;
+                    }
+                }
+
+                // only one character different
+                if (diff == 1) {
+
+                    adj.add(new Pair(a, b, 0));
+                    adj.add(new Pair(b, a, 0));
+                }
+            }
+        }
+
+        // BFS
+        Queue<Pair> q = new LinkedList<>();
+        HashSet<String> visited = new HashSet<>();
+
+        visited.add(beginWord);
+
+        // add all neighbors of beginWord
+        for (int i = 0; i < adj.size(); i++) {
+
+            Pair p = adj.get(i);
+
+            if (p.first.equals(beginWord)) {
+
+                visited.add(p.second);
+                q.offer(new Pair(p.first, p.second, 1));
+            }
+        }
+
+        while (!q.isEmpty()) {
+
+            Pair curr = q.poll();
+
+            String a = curr.first;
+            String b = curr.second;
+            int steps = curr.steps;
+
+            // found answer
+            if (b.equals(endWord)) {
+                return steps + 1;
             }
 
-            for(int i = 0; i<word.length(); i++){
-                char[] replacedCharArray = word.toCharArray();
-                for(char ch = 'a'; ch <='z'; ch++){
-                    replacedCharArray[i] = ch;
+            // iterate entire adjacency list
+            for (int i = 0; i < adj.size(); i++) {
 
-                    String newWord = new String(replacedCharArray);
-                    if(set.contains(newWord)){
-                        queue.offer(new Pair(newWord, step+1));
-                        set.remove(newWord);
+                Pair p = adj.get(i);
+
+                // neighbor of current node
+                if (b.equals(p.first)) {
+
+                    if (!visited.contains(p.second)) {
+
+                        visited.add(p.second);
+
+                        q.offer(new Pair(
+                            p.first,
+                            p.second,
+                            steps + 1
+                        ));
                     }
                 }
             }
-            
         }
-    return 0;
 
+        return 0;
     }
 }
