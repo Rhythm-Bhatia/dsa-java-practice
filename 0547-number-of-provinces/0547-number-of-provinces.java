07b0554b285af8b6a1def56fc1761edc5d421832
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-05-15 20:31:40 +0530

    Time: 3 ms (27.39%), Space: 50.1 MB (5.48%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,22 +1,34 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int count=0;
-        boolean[]visited=new boolean[isConnected.length];
+        List<List<Integer>>list=new ArrayList<>();
         for(int i=0;i<isConnected.length;i++){
-            if(!visited[i]){
-                dfs(i,isConnected,visited);
-                count++;
-            }
+            list.add(new ArrayList<>());
         }
-        return count;
-    }
-    private void dfs(int i,int[][]isConnected,boolean[]visited){
-        visited[i]=true;
-        for(int j=0;j<isConnected[0].length;j++){
-            if(isConnected[i][j]==1&&!visited[j]){
-                dfs(j,isConnected,visited);
-            }
-        }
-    }
+        for(int i=0;i<isConnected.length;i++){
+            for(int j=0;j<isConnected[0].length;j++){
+                if(isConnected[i][j]==1&&i!=j){
+                    list.get(i).add(j);
+                }
 
+            }
+        }
+        boolean visited[]=new boolean[list.size()];
+        int num=0;
+        for(int i=0;i<list.size();i++){
+            if(visited[i]==false){
+                dfs(list,visited,i);
+                num++;
+            }
+        }
+        return num;
+    }
+    void dfs(List<List<Integer>>list,boolean[]visited,int i){
+        visited[i]=true;
+        for(int a:list.get(i)){
+            if(visited[a]==false){
+                dfs(list,visited,a);
+            }
+        }
+
+    }
 }
