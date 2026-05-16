96411678df14f4cfa77944436f8a8cfdd649be9c
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-05-16 12:26:05 +0530

    Time: 6 ms (69.11%), Space: 46.3 MB (47.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,33 +1,27 @@
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         int n=s.length();
-//         int count=0;
-//         Set<Character>set=new LinkedHashSet<>();
-//         for(int i=0;i<n;i++){
-//             while(set.contains(s.charAt(i))){
-//                 char first = set.iterator().next(); 
-//                 set.remove(first); 
-//             }
-//             set.add(s.charAt(i));
-//             count=Math.max(count,set.size());
-//         }
-//         return count;
-//     }
-// }
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int l = 0, maxLen = 0;
-        Set<Character> set = new HashSet<>();
-
-        for (int r = 0; r < n; r++) {
-            while (set.contains(s.charAt(r))) {
-                set.remove(s.charAt(l));
-                l++;
+        Map<Character,Integer>map=new HashMap<>();
+        char arr[]=s.toCharArray();
+        int max=0;
+        int k=0;
+        for(int i=0;i<arr.length;i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i],i);
+                
             }
-            set.add(s.charAt(r));
-            maxLen = Math.max(maxLen, r - l + 1);
+            else{
+                int till=map.get(arr[i]);
+                for(int j=k;j<=till;j++){
+                    map.remove(arr[j]);
+
+                }
+                k=till+1;
+                map.put(arr[i],i);
+            }
+            max=Math.max(map.size(),max);
+
+            
         }
-        return maxLen;
+        return max;
     }
 }
