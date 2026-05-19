3db2632fe63bb3b96d0e2f872769ebfb2254d5a2
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-05-20 00:33:16 +0530

    Time: 15 ms (54.64%), Space: 46.4 MB (99.94%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int result[]=new int[nums.length];
+        int n=nums.length;
+        Stack<Integer>stack=new Stack<>();
+        Arrays.fill(result,-1);
+        for(int i=2*n-1;i>=0;i--){
+            int current=nums[i%n];
+            while(!stack.isEmpty()&&stack.peek()<=current){
+                stack.pop();
+            }
+            if(i<n){
+                if(!stack.isEmpty()){
+                result[i%n]=stack.peek();
+                }
+            }
+            
+            
+            stack.push(current);
+        }
+        return result;
+    }
+}
