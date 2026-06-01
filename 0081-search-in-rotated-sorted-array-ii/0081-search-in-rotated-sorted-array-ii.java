e58f2caa11e584ea22df2c83499d2f4b23335399
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-06-01 17:15:41 +0530

    Time: 0 ms (100%), Space: 45 MB (44.36%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -10,7 +10,7 @@ class Solution {
                 high--;
                 continue;
             }
-            else if(nums[low]<=nums[mid]){
+            if(nums[low]<=nums[mid]){
                 if(nums[low]<=target&&target<=nums[mid]){
                     high=mid-1;
                 }
