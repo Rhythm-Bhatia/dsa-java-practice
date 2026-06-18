2c7d15a865a86dd52d7f27f9e8b24c85861b9dfc
Rhythm Bhatia <rhythmbhatia19@gmail.com>
2026-06-18 15:23:35 +0530

    Time: 275 ms (12.31%), Space: 47.7 MB (23.08%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.java
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.java
@@ -1,53 +1,74 @@
 class Solution {
     public String largestPalindromic(String num) {
-         TreeMap<Character,Integer>map=new TreeMap<>(Collections.reverseOrder());
-         for(int i=0;i<num.length();i++){
-            map.put(num.charAt(i),map.getOrDefault(num.charAt(i),0)+1);
-         }
-         String str1="";
-         String str2="";
-         char max='#';
-         for(Map.Entry<Character,Integer> entry:map.entrySet()){
-            char key=entry.getKey();
-            int value=entry.getValue();
-            if(value%2!=0&&max=='#'){
-                max=key;
 
+        TreeMap<Character, Integer> map =
+                new TreeMap<>(Collections.reverseOrder());
+
+        for (int i = 0; i < num.length(); i++) {
+            map.put(num.charAt(i),
+                    map.getOrDefault(num.charAt(i), 0) + 1);
+        }
+
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+
+        char max = '#';
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+
+            char key = entry.getKey();
+            int value = entry.getValue();
+
+            if (value % 2 != 0 && max == '#') {
+                max = key;
             }
-            int count=value/2;
-            while(count!=0){
-                str1=str1+key;
-                str2=key+str2;
+
+            int count = value / 2;
+
+            while (count != 0) {
+                str1.append(key);
+                str2.insert(0, key);   // same as your key + str2
                 count--;
             }
-         }
-         if(max!='#'){
-            str1=str1+max;
-         }
-         str1=str1+str2;
-         if(str1.isEmpty()){
+        }
+
+        if (max != '#') {
+            str1.append(max);
+        }
+
+        str1.append(str2);
+
+        String ans = str1.toString();
+
+        if (ans.isEmpty()) {
             return "0";
-         }
-         int i=0;
-         int j=str1.length()-1;
-         while(j>i&&(str1.charAt(i)=='0'||str1.charAt(j)=='0')){
-            if(str1.charAt(i)=='0'){
+        }
+
+        int i = 0;
+        int j = ans.length() - 1;
+
+        while (j > i &&
+              (ans.charAt(i) == '0' || ans.charAt(j) == '0')) {
+
+            if (ans.charAt(i) == '0') {
                 i++;
             }
-            if(str1.charAt(j)=='0'){
+
+            if (ans.charAt(j) == '0') {
                 j--;
             }
-         }
-         if(i>j){
+        }
+
+        if (i > j) {
             return "0";
-         }
-         str1= str1.substring(i,j+1);
-         if(str1.isEmpty()){
+        }
+
+        ans = ans.substring(i, j + 1);
+
+        if (ans.isEmpty()) {
             return "0";
-         }
-         else{
-            
-            return str1;
-         }
+        }
+
+        return ans;
     }
 }
